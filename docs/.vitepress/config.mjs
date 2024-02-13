9ae510ca6d1ce8bdf2f18a0f75cf4e439a17b397
Teszáry Péter <bsze2tespet@vasvari.org>
2024-02-13 22:22:02 +0200

    updated config.mjs

--- docs/.vitepress/config.mjs
+++ docs/.vitepress/config.mjs
@@ -2,6 +2,7 @@ import { defineConfig } from 'vitepress'
 
 // https://vitepress.dev/reference/site-config
 export default defineConfig({
+  base: '/commerce-sync-docs/',
   title: "Commerce Sync Docs",
   description: "A Projektünk dokumentációs oldala",
   themeConfig: {
